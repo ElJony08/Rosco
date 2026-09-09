1976f163712f6f55cdc059e6c6e546890898aa81
ElJony08 <jonvillafruela88@gmail.com>
2026-09-09 13:14:34 +0200

    Update questions and answers in script.js

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ const preguntas = [
   { letra: "D", pregunta: "Con la D: En qué juego no le tenías fe a Markel y él lo consiguió", respuesta: "Dardos" },
   { letra: "E", pregunta: "Con la E: la mujer más facha de todo Torrevieja, su toalla es prueba de ello", respuesta: "Encarna" },
   { letra: "F", pregunta: "Con la F: Comido que descubrió Endika por nosotros", respuesta: "Fideuá" },
-  { letra: "G", pregunta: "Empieza por G: Jon", respuesta: "gay" },
-  { letra: "H", pregunta: "Empieza por H: Planta con la que se hace el aceite de oliva.", respuesta: "higuera" },
+  { letra: "G", pregunta: "Con la G: Jon", respuesta: "gay" },
+  { letra: "H", pregunta: "Contiene la H: A Markel y a ti se os quedaba pegada en Torrevieja, a veces demasiado", respuesta: "almohada" },
   { letra: "I", pregunta: "Contiene la I: El proceso de subir el alimento para masticarlo otra vez", respuesta: "rumiar" },
-  { letra: "J", pregunta: "Empieza por J: El vigilante del armero", respuesta: "Jabalí" },
-  { letra: "K", pregunta: "Contiene la K: Deporte de combate de origen japonés.", respuesta: "karate" },
+  { letra: "J", pregunta: "Con la J: El vigilante del armero", respuesta: "Jabalí" },
+  { letra: "K", pregunta: "Con la K: No sabemos cómo haces para quedar siempre arriba en el", respuesta: "ranking" },
   { letra: "L", pregunta: "Con la L: Quién le pidió el móvil a Markel para llamar a quién", respuesta: "Luis" },
   { letra: "M", pregunta: "Con la M: Lugar donde descubrimnos que es mejor no darte un hacha", respuesta: "Max Center" },
-  { letra: "N", pregunta: "Termina en N: El examen que se le hace a un aniamal despues de morir para determinar la causa de la muerte", respuesta: "Necropsia" },
+  { letra: "N", pregunta: "Con la N: El examen que se le hace a un aniamal despues de morir para determinar la causa de la muerte", respuesta: "Necropsia" },
   { letra: "Ñ", pregunta: "Contiene la Ñ: Tocar tetas, pero a las vacas", respuesta: "ordeñar" },
   { letra: "O", pregunta: "Con la O: Por lo que pareces de otra raza", respuesta: "ojos" },
-  { letra: "P", pregunta: "Empieza por P: Es el regalo de tu cumpleaños🫢", respuesta: "madrid" },
+  { letra: "P", pregunta: "Con la P: Es el regalo de tu cumpleaños🫢", respuesta: "madrid" },
 ];
 
 let indiceActual = 0;
